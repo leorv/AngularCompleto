237dd34bd6b347df2031423ff1012efc7d14a6b1
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2021-12-26 22:56:05 -0300

    Criando o módulo cursos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
+
+
+
 @NgModule({
-  declarations: [
+  declarations: [ // Componentes, diretivas e pipes.
     AppComponent,
     MeuPrimeiroComponent,
     MeuPrimeiro2Component
   ],
-  imports: [
+  imports: [ // Outros módulos
     BrowserModule,
+    CursosModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [], // Serviços que ficarão disponíveis.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
