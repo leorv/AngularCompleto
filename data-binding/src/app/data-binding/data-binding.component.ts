9393239761cf35bad812809f7c6bd06b80d0c887
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-01-22 20:24:39 -0300

    Event binding, examples with click, keyup, keyup.enter, blur, mouseover, mouseout.

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,10 @@ export class DataBindingComponent implements OnInit {
     url: string = 'https://loiane.training';
     imgUrl: string = 'https://picsum.photos/200/300';
     cursoAngular: boolean = true;
+    valorAtual: string = '';
+    valorSalvo: string = '';
+
+    isMouseOver: boolean = false;
 
 
     getCurtirCurso() {
@@ -19,6 +23,26 @@ export class DataBindingComponent implements OnInit {
 
     getValor() {
         return 1;
+    }
+
+    botaoClicado() {
+        alert('Botão clicado!')
+    }
+
+    onkeyup(evento: Event) {
+        console.log("onkeyup: ", evento);
+        // console.log((<HTMLInputElement>evento.target).value);
+        this.valorAtual = (<HTMLInputElement>evento.target).value;
+        // this.valorAtual = texto;
+    }
+
+    salvarValor(evento: any){
+        console.log("salvarValor: ", evento);
+        this.valorSalvo = evento;
+    }
+
+    onMouseOverOut() {
+        this.isMouseOver = !this.isMouseOver;
     }
 
     constructor() { }
