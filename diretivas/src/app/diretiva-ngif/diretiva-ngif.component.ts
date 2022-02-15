fa9d5dd23865aca9ab4dd4c744bec8c411ca98f5
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-02-15 18:50:23 -0300

    diretiva ngif

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-diretiva-ngif',
-  templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+    selector: 'app-diretiva-ngif',
+    templateUrl: './diretiva-ngif.component.html',
+    styleUrls: ['./diretiva-ngif.component.css']
 })
 export class DiretivaNgifComponent implements OnInit {
 
-  constructor() { }
+    cursos: string[] = [];
+    mostrarCursos: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onMostrarCursos() {
+        this.mostrarCursos = !this.mostrarCursos;
+    }
 
 }
