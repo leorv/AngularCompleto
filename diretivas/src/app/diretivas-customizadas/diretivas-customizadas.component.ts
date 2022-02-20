f21f36548029a92f1e6854b3dd58bcda0e08fc3d
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-02-19 23:30:59 -0300

    creating a ng-else, diretiva estruturada.

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-diretivas-customizadas',
-  templateUrl: './diretivas-customizadas.component.html',
-  styleUrls: ['./diretivas-customizadas.component.css']
+    selector: 'app-diretivas-customizadas',
+    templateUrl: './diretivas-customizadas.component.html',
+    styleUrls: ['./diretivas-customizadas.component.css']
 })
 export class DiretivasCustomizadasComponent implements OnInit {
+    mostrarCursos: boolean = false;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    onMostrarCursos() {
+        this.mostrarCursos = !this.mostrarCursos;
+    }
 
 }
