9c4fa6286fdb75af92780c6f5272a1ea63c2b180
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-02-27 20:01:51 -0300

    Pipe async (assíncrono)

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'app-exemplos-pipes',
@@ -39,6 +41,16 @@ export class ExemplosPipesComponent implements OnInit {
         dataLancamento: new Date(2019, 3, 11),
         url: 'https://www.amazon.com.br/Estruturas-Dados-Algoritmos-Com-Javascript/dp/8575226932'
     }
+
+    // Na programação assíncrona, você pode decidir o que usar, se promisses ou observables.
+
+    valorAsync = new Promise((resolve, reject) => {
+        setTimeout(() => {
+           resolve('Valor assíncrono');
+        }, 2000);
+    });
+
+    valorAsync2 = interval(2000).pipe(map(value => 'Valor assíncrono 2'));
 
     constructor() { }
 
