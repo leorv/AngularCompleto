89c927873881ae18c2a1a840e4e10a66273254a2
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-05 19:52:51 -0300

    definindo e extraindo parâmetros de URL, ex: proxima pagina, queryParams

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -24,6 +24,7 @@ export class CursoDetalheComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
+        console.log(this.route.params);
         this.inscricao = this.route.params.subscribe(
             (params: any) => {
                 this.id = params['id'];
--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 import { CursosService } from '../cursos/cursos.service';
 
@@ -8,14 +11,32 @@ import { CursosService } from '../cursos/cursos.service';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+    pagina: number = 0;
     cursos: any[] = [];
+    inscricao: Subscription = new Subscription();
 
   constructor(
-      private cursosService: CursosService
+      private cursosService: CursosService,
+      private route: ActivatedRoute,
+      private router: Router
   ) { }
 
   ngOnInit(): void {
       this.cursos = this.cursosService.getCursos();
+      this.inscricao = this.route.queryParams.subscribe(
+          (queryParams: any) => {
+              this.pagina = queryParams['pagina'];
+          }
+      );
+  }
+
+  ngOnDestroy(): void {
+      this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+      // this.pagina++;
+      this.router.navigate(['/cursos'], {queryParams: {pagina: ++this.pagina} });
   }
 
 }
