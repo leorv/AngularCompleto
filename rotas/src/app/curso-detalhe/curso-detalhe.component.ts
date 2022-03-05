03795e58de9d3288783e5bb9fab72ffe63e0e10e
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-05 15:43:13 -0300

    escutando mudanças nos parâmetros de roteamento, usando subscribe

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-curso-detalhe',
     templateUrl: './curso-detalhe.component.html',
     styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent implements OnInit {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
+
+    inscricao: Subscription = new Subscription();
 
     id: string = '';
 
     constructor( private route: ActivatedRoute) {
         // console.log(this.route); <- apenas para descobrir como ele se apresenta como objeto e seus parâmetros.
-        this.id = this.route.snapshot.params['id'];
+        // this.id = this.route.snapshot.params['id'];
      }
 
     ngOnInit(): void {
+        this.inscricao = this.route.params.subscribe(
+            (params: any) => {
+                this.id = params['id'];
+            }
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.inscricao.unsubscribe();
     }
 
 }
