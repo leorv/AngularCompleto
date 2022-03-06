79b784126869abee284187a7704df1d850644af5
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-06 09:16:39 -0300

    criando rotas filhas: children

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { AlunosModule } from './alunos/alunos.module';
 
 
 @NgModule({
@@ -28,6 +29,7 @@ import { MatListModule } from '@angular/material/list';
         AppRoutingModule,
         BrowserAnimationsModule,
 
+        AlunosModule,
         CursosModule,
 
         MatIconModule,
