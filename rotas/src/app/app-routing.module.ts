77ea01ead99462fd5f2a11c2d2d8502b375984d2
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-25 17:49:23 -0300

    estilo de URL: HTML 5 ou usando # hash

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -3,34 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
-import { AlunosGuard } from './guards/alunos.guard';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 const routes: Routes = [
-    { path: 'cursos',
+    {
+        path: 'cursos',
         loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule),
         canActivate: [AuthGuard],
         canActivateChild: [CursosGuard],
         canLoad: [AuthGuard]
     },
-    { path: 'alunos', 
+    {
+        path: 'alunos',
         loadChildren: () => import('./alunos/alunos.module').then(mod => mod.AlunosModule),
         canActivate: [AuthGuard],
         //canActivateChild: [AlunosGuard]
         canLoad: [AuthGuard]
     },
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', component: PaginaNaoEncontradaComponent}
+    { path: '**', component: PaginaNaoEncontradaComponent }
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes),
+        RouterModule.forRoot(routes, { useHash: true }),
     ],
     exports: [RouterModule]
 })
