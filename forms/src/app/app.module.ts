2a914816bd1f7d18762b08fa53b354145762ca43
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-25 20:59:42 -0300

    criando um formulario simples com o bootstrap

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    DataFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
