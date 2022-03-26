6b732554ae8db0b1e5dd7ccc256dbfb02a73078f
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-26 07:39:13 -0300

    Modulos e FormsModule

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -5,20 +6,19 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
