f7d4f479138f368caf3fb821fb200f27e8776534
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-29 10:40:46 -0300

    populando campos do form automaticamente com setValue e patchValue (autocomplete CEP)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -39,21 +39,67 @@ export class TemplateFormComponent implements OnInit {
         }
     }
 
-    consultaCEP(cep: any){
+    consultaCEP(cep: any, form: any) {
         console.log(cep);
 
         cep = cep.replace(/\D/g, '');
 
-        if (cep != ""){
+
+        if (cep != "") {
             var validaCEP = /^[0-9]{8}$/;
 
-            if (validaCEP.test(cep)){
+            this.resetaDadosFormulario(form);
+
+            if (validaCEP.test(cep)) {
                 // this.http.get("//viacep.com.br/ws/" + cep + "/json"); <-- sem ECMA5
                 // Abaixo a concatenação com ECMAScript 5
                 this.http.get(`//viacep.com.br/ws/${cep}/json`)
                     .pipe(map((dados: any) => dados))
-                    .subscribe(dados => console.log(dados));
+                    .subscribe(dados => {
+                        console.log(dados);
+                        this.populaDadosForm(dados, form)
+                    });
             }
         }
+    }
+
+    populaDadosForm(dados: any, formulario: any) {
+        console.log(formulario);
+        // formulario.setValue({
+        //     nome: formulario.value.nome,
+        //     email: formulario.value.email,
+        //     endereco: {
+        //         cep: dados.cep,
+        //         complemento: dados.complemento,
+        //         rua: dados.logradouro,
+        //         numero: formulario.value.endereco.numero,
+        //         bairro: dados.bairro,
+        //         cidade: dados.localidade,
+        //         estado: dados.uf
+        //     }
+        // });
+
+        formulario.form.patchValue({
+            endereco: {
+                cep: dados.cep,
+                complemento: dados.complemento,
+                rua: dados.logradouro,
+                bairro: dados.bairro,
+                cidade: dados.localidade,
+                estado: dados.uf
+            }
+        })
+    }
+
+    resetaDadosFormulario(formulario: any){
+        formulario.form.patchValue({
+            endereco: {
+                complemento: null,
+                rua: null,
+                bairro: null,
+                cidade: null,
+                estado: null
+            }
+        })
     }
 }
