bbaf7bf6e70794e8d1a139e51389742d0e73f779
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-03-30 00:05:50 -0300

    Formularios Reativos - Reactive Forms, iniciando, modulo e componente.

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataFormComponent } from './data-form/data-form.component';
+import { DataFormModule } from './data-form/data-form.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataFormComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    DataFormModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     TemplateFormModule
   ],
--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-data-form',
-  templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+    selector: 'app-data-form',
+    templateUrl: './data-form.component.html',
+    styleUrls: ['./data-form.component.css']
 })
 export class DataFormComponent implements OnInit {
 
-  constructor() { }
+    formulario: FormGroup = new FormGroup({});
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
