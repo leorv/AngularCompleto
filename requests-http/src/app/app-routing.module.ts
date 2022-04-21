10706f5abc31a988d3b592530935a6cc6ca71cf6
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-04-21 20:05:03 -0300

    Adicionando bootstrap, criando o módulo cursos com seu roteamento e atualizando o app routing para lazy loading.

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '', pathMatch: 'full', redirectTo: 'cursos'
+    },
+    {
+        path: 'cursos',
+        loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule)
+        // antes do Angular 5, colocávamos o caminho com app/, depois na v6
+        // usamos somente o ./
+        //'./cursos/cursos.module#CursosModule'
+        // mas agora não recebe mais string, e sim loadChildrenCallback
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
