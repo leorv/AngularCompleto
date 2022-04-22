7461c5e7f1b1fc08fba18c7c2fcc06043703d7ec
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-04-21 22:45:48 -0300

    Http GET: listar registros, cursosService.

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
-  selector: 'app-cursos-lista',
-  templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+    selector: 'app-cursos-lista',
+    templateUrl: './cursos-lista.component.html',
+    styleUrls: ['./cursos-lista.component.scss'],
+    preserveWhitespaces: true
 })
 export class CursosListaComponent implements OnInit {
 
-  constructor() { }
+    cursos: Curso[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(private cursoService: CursosService) { }
+
+    ngOnInit(): void {
+        this.cursoService.list().subscribe(
+            dados => this.cursos = dados
+        );
+    }
 
 }
