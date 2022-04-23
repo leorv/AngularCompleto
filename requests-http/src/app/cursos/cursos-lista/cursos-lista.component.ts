3ce4bcd53f3364902a1c40618cd6616eadb0683b
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-04-22 23:53:34 -0300

    Http GET + Pipe Async

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -10,14 +11,18 @@ import { CursosService } from '../cursos.service';
 })
 export class CursosListaComponent implements OnInit {
 
-    cursos: Curso[] = [];
+    // Uma prática adotada pela comunidade é a notação finlandesa,
+    // colocando um dólar no final da variável, significa que
+    // ela é um observable.
+    cursos$: Observable<Curso[]> = new Observable();
 
     constructor(private cursoService: CursosService) { }
 
     ngOnInit(): void {
-        this.cursoService.list().subscribe(
-            dados => this.cursos = dados
-        );
+        // this.cursoService.list().subscribe(
+        //     dados => this.cursos = dados
+        // );
+        this.cursos$ = this.cursoService.list();
     }
 
 }
--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { delay, tap } from 'rxjs';
 
 import { environment } from './../../environments/environment';
 import { Curso } from './curso';
@@ -17,6 +17,10 @@ export class CursosService {
     list() {
         return this.http.get<Curso[]>(this.API)
             .pipe(
+                // Utilizaremos esse delay apenas para simular que a requisição está
+                // sendo feito e existe um tempo para o servidor retornar, fazendo a
+                // gente implementar uma mensagem de carregamento no template.
+                delay(2000),
                 tap(console.log)
             );
     }
