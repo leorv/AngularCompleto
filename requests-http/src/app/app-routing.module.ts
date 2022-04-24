4c1362aab02f8b456f59fe2e67c925d2425f1ae1
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-04-24 19:00:13 -0300

    RxJS poc

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -3,20 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        path: '', pathMatch: 'full', redirectTo: 'cursos'
-    },
-    {
         path: 'cursos',
         loadChildren: () => import('./cursos/cursos.module').then(mod => mod.CursosModule)
         // antes do Angular 5, colocávamos o caminho com app/, depois na v6
         // usamos somente o ./
         //'./cursos/cursos.module#CursosModule'
         // mas agora não recebe mais string, e sim loadChildrenCallback
+    },
+    {
+        // TODO:inserir o componente corretamente no lazy loading.
+        path: 'rxjs-poc', loadChildren: () => import('../app/unsubscribe-rxjs/unsubscribe-rxjs.module').then(mod => mod.UnsubscribeRxjsModule)
+    },
+    {
+        path: '', pathMatch: 'full', redirectTo: 'cursos'
     }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
