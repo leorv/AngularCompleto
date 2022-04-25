1599d3732de263490895437622437349b0a0a0b0
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-04-24 22:56:07 -0300

    Capturando Erros (+ Erro com async)

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,5 +1,6 @@
+import { switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, EMPTY, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -16,13 +17,30 @@ export class CursosListaComponent implements OnInit {
     // ela é um observable.
     cursos$: Observable<Curso[]> = new Observable();
 
+    error$ = new Subject<boolean>();
+
     constructor(private cursoService: CursosService) { }
 
     ngOnInit(): void {
         // this.cursoService.list().subscribe(
         //     dados => this.cursos = dados
         // );
-        this.cursos$ = this.cursoService.list();
+        this.onRefresh();
+    }
+
+    onRefresh() {
+        this.cursos$ = this.cursoService.list()
+            .pipe(
+                // map(),
+                // tap(), a lógica do erro é legal deixarmos por último, para ele pegar qualquer erro que ocorre aqui antes.
+                // switchMap()
+                catchError(error => {
+                    console.error(error);
+                    this.error$.next(true);
+                    return EMPTY;
+                    // return of(); // isso aqui daria o mesmo resultado dali de cima.
+                })
+            );
     }
 
 }
