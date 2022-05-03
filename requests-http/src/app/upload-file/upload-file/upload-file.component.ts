70f9706102a43f8759dbb963c06d2469fe463f34
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-05-03 12:09:10 -0300

    Http Upload de Arquivo: Request com FormData

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
     selector: 'app-upload-file',
     templateUrl: './upload-file.component.html',
     styleUrls: ['./upload-file.component.scss']
 })
-export class UploadFileComponent implements OnInit {
+export class UploadFileComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    files: Set<File> = new Set<File>();
+    subcription: Subscription = new Subscription();
+
+    constructor(private uploadFileService: UploadFileService) { }
 
     ngOnInit(): void {
     }
@@ -24,7 +29,25 @@ export class UploadFileComponent implements OnInit {
         // Porém, em versões anteriores isso não acontece. Então faria o seguinte, dando um id para
         // o elemento input no template e:
         // document.getElementById('customFieldLabel')?.innerHTML = selectedFiles[0].name;
-        
+
+        for (let i = 0; i < selectedFiles.length; i++){
+            this.files.add(selectedFiles[i]);
+        }
+
+    }
+
+    onUpload() {
+        if (this.files && this.files.size > 0){
+            this.subcription = this.uploadFileService.upload(this.files, 'http://localhost:8000/upload')
+                .subscribe({
+                    next: response => console.log('upload concluído.'),
+                    error: err => console.log('Ocorreu um erro ao fazer upload. Tente novamente.')
+                });
+        }
+    }
+
+    OnDestroy(){
+        this.subcription.unsubscribe();
     }
 
 }
