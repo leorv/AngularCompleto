b5ec18d6cdf3cc3ae803ec66823857ed9a9cf1ee
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-12-05 23:24:02 -0300

    Service passando um Observable, com chamada get.

--- 10-crud-angular/crud-angular/src/app/courses/courses/courses.component.ts
+++ 10-crud-angular/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
@@ -10,13 +11,31 @@ import { Component, OnInit } from '@angular/core';
 export class CoursesComponent implements OnInit {
 
     // Não é recomendado tipar como any.
-    courses: Course[] = [];
+
+    /*
+    O angular Material pode receber um array ou um Observable na Table.
+    Assim, ele trata esse observable.
+    De outra forma, teríamos que fazer um subscribe, um ng-for, 
+    .list().subscribe(courses => this.courses = courses)
+    Se nossa variável fosse um array de cursos tá.
+
+    Outra forma seria usar o pipe async no template no ng-for. Aí ele
+    faz o subscribe automático.
+
+    Sempre que possível, a gente evita fazer o subscribe e deixa o Angular tratar disso
+    ele mesmo.
+    */
+
+
+    courses: Observable<Course[]>;
     displayedColumns = ['name', 'category'];
 
-    constructor(private coursesService: CoursesService) { }
+    constructor(private coursesService: CoursesService) {
+        this.courses = this.coursesService.list();
+     }
 
     ngOnInit(): void {
-        this.courses = this.coursesService.list();
+        
     }
 
 }
--- 10-crud-angular/crud-angular/src/app/courses/services/courses.service.ts
+++ 10-crud-angular/crud-angular/src/app/courses/services/courses.service.ts
@@ -1,19 +1,28 @@
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 
 import { Course } from './../model/course';
 import { HttpClient } from '@angular/common/http'
+import { first, Observable, take, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CoursesService {
 
+    private readonly API: string = '/assets/courses.json';
+
     constructor(private http: HttpClient) { }
 
-    list(): Course[] {
-        return [
-            { _id: '1', name: 'Angular', category: 'front-end' },
-            { _id: '2', name: 'Java', category: 'back-end' }
-        ]
+    list(): Observable<Course[]> {
+        return this.http.get<Course[]>(this.API)
+            .pipe(
+                first(),
+                // take(1),
+                // O first obtém apenas a primeira resposta que o servidor enviar.
+                // Como neste caso o servidor não é nenhum stream de dados, basta
+                // pegarmos os cursos uma única vez quando atualizarmos a página.
+                tap(courses => console.log(courses))
+            );
     }
 }
